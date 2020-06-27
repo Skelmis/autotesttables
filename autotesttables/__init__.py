be87a31086869cee4afc7b2b26330c538ce98d3a
skelmis <skelmis.craft@gmail.com>
2020-06-27 15:57:27 +1200

    .

--- autotesttables/__init__.py
+++ autotesttables/__init__.py
@@ -1,7 +1,2 @@
-<<<<<<< HEAD
 from autotesttables.table import Table
 from autotesttables.generator import Generator
-=======
-from autotesttables.table import Table
-from autotesttables.generator import Generator
->>>>>>> 495de00... .
